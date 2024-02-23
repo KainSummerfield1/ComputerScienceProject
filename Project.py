1ea1c2bebbdcdda30d3be66250da8b04354d8f96
KainSummerfield1 <156071862+KainSummerfield1@users.noreply.github.com>
2024-02-23 10:36:04 +0000

    Updated Project.py

--- Project.py
+++ Project.py
@@ -1,1 +1,15 @@
-W
+import random
+
+login1 = ("user1:password1")
+
+
+loginuserinput = ()
+loginuserinput = input("Please enter your login (must be seperated with a colon) : ")
+
+while login1 !=loginuserinput:
+    print("wrong login! try again!")
+    loginuserinput = input("Please enter your login (must be seperated with a colon) : ")
+print("game starting!")
+    
+        
+file = open("songs.txt","r")
