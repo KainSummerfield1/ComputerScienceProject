291ce6acd8d380f7bb670acbbeaaffabacb96886
KainSummerfield1 <156071862+KainSummerfield1@users.noreply.github.com>
2024-03-01 10:30:20 +0000

    Updatec Project.py

--- Project.py
+++ Project.py
@@ -1,35 +1,49 @@
 import random
-username = "user1"
-password = "password"
-song = ()
+
+songs = [
+"song1 - artist 1",
+"song2 - artist 2",
+"song3 - artist 3",
+"song4 - artist 4",
+"song5 - artist 5",
+]
+
+username = "a"
+password = "a"
+songguess = ()
+songanswer = ()
 userguess1 = ()
 userguess2 = ()
 score = ()
 usernameuserinput = ()
 usernameuserinput = input("Please enter your username: ")
 
+print("  ")
+
 passworduserinput = ()
 passworduserinput = input("Please enter your password: ")
 
 while usernameuserinput != username:
     print("Username or Password Incorrect! Try Again.")
-    usernameuserinput = input(
-        "Please enter your username: ")
+    usernameuserinput = input("Please enter your username: ")
+    print("  ")
     passworduserinput = input("Please enter your password: ")
 
 while passworduserinput != password:
     print("Username or Password Incorrect! Try Again.")
-    usernameuserinput = input(
-        "Please enter your username: ")
+    usernameuserinput = input("Please enter your username: ")
+    print("  ")
     passworduserinput = input("Please enter your password: ")
+print("  ")
 
-print ("Game Starting...")
+print("Game Starting...")
 
-print("There will be one letter and the Artist's name. You have to try and guess the song name")
+print("  ")
 
-song = random.choice(open('songs.txt').readlines())
+print(
+    "There will be one letter and the Artist's name. You have to guess the song name."
+)
 
-print(song)
+print("  ")
 
-
-
+print(random.choice(songs))
