ad3afd083d11d5ce1a7987eac033bb6a61f74567
KainSummerfield1 <156071862+KainSummerfield1@users.noreply.github.com>
2024-03-08 10:34:32 +0000

    Updated Project.py

--- Project.py
+++ Project.py
@@ -1,13 +1,13 @@
 import random
-import sys
+
 # importing random for chosing random song
 
 songs = [
-"1test",
-"2test",
-"3test",
-"4test",
-"5test",
+"1test - artist1",
+"2test - artist2",
+"3test - artist3",
+"4test - artist4",
+"5test - artist5",
 ]
 
 # list of songs
@@ -35,7 +35,7 @@ while usernameuserinput != username:
     usernameuserinput = input("Please enter your username: ")
     print("  ")
     passworduserinput = input("Please enter your password: ")
-    
+
 # makes sure that the username + password is correct
 
 while passworduserinput != password:
@@ -43,7 +43,7 @@ while passworduserinput != password:
     usernameuserinput = input("Please enter your username: ")
     print("  ")
     passworduserinput = input("Please enter your password: ")
-    
+
 print("  ")
 
 print("Game Starting...")
@@ -54,7 +54,6 @@ print(
     "There will be one letter and the Artist's name. You have to guess the song name."
 )
 
-print("  ")
 
 songguess = random.choice(songs)
 
@@ -71,12 +70,33 @@ if userguess1 != songguess:
     print("  ")
     userguess2 = input("What do you think the song is? : ")
     print("  ")
+    if userguess2 == songguess:
+         print("Correct! Moving onto the next round...")
     if userguess2 !=songguess:
         print("Incorrect! Game Over!")
-        sys.exit()
-        
+          
+
 
 elif userguess1 == songguess:
     print("")
     print("Correct! Moving onto the next round...")
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
