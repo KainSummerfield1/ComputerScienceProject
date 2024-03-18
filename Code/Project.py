c29c8d081b8a10821c006fb1e677dbd910f0d47d
KainSummerfield1 <156071862+KainSummerfield1@users.noreply.github.com>
2024-03-18 12:04:07 +0000

    Update Project.py

--- Code/Project.py
+++ Code/Project.py
@@ -1,17 +1,17 @@
 
 import random
-import time 
-#import pyotp 
- 
+import time
+import pyotp
+
 
 # importing random for chosing random song
 
 songs = [
-"1test - artist1",
-"2test - artist2",
-"3test - artist3",
-"4test - artist4",
-"5test - artist5",
+"1test-artist1",
+"2test-artist2",
+"3test-artist3",
+"4test-artist4",
+"5test-artist5",
 ]
 
 # list of songs
@@ -26,10 +26,11 @@ score = ()
 usernameuserinput = ()
 key = "NNQWS3TTOVWW2ZLSMZUWK3DE"
 googleauth = ()
-  
-#uri = pyotp.totp.TOTP(key).provisioning_uri( 
-    #name='Kain', 
-    #issuer_name="Kain's Music Game")
+song = ("")
+
+uri = pyotp.totp.TOTP(key).provisioning_uri(
+    name='Kain',
+    issuer_name="Kain's Music Game")
 
 # required variables
 
@@ -57,53 +58,63 @@ while passworduserinput != password:
 
 print("  ")
 #Google Authenticator OTP
-#totp = pyotp.TOTP(key)
-#two_factor_code = totp.now()
-#googleauth = input("Please enter your Google Authenticator Code : ")
+totp = pyotp.TOTP("NNQWS3TTOVWW2ZLSMZUWK3DE")
+two_factor_code = totp.now()
+googleauth = input("Please enter your Google Authenticator Code : ")
 
-#while googleauth != two_factor_code:
-    #print("Code Invalid. Try Again.")
-    #print("")
-    #googleauth = input("Please enter your Google Authenticator Code : ")
-    
+while googleauth != two_factor_code:
+    print("Code Invalid. Try Again.")
+    print("")
+    googleauth = input("Please enter your Google Authenticator Code : ")
+
 print("   ")
 
 print("Game Starting...")
+while userguess2 != song:
+    print("  ")
 
-print("  ")
-
-print(
+    print(
     "There will be one letter and the Artist's name. You have to guess the song name."
-)
+    )
 
-print("   ")
-
-songguess = random.choice(songs)
-
-artist = songguess.split("-")
-song = songguess.split("-")
-print(artist[1])
-
-print(song[0])
-
-# picks a random song and displays the first letter
-
-print("   ")
-
-userguess1 = input("What do you think the song is? : ")
-
-if userguess1 != song:
-    print("Incorrect! You have one more chance!")
-    print("  ")
-    userguess2 = input("What do you think the song is? : ")
-    print("  ")
-    if userguess2 == song:
-         print("Correct! Moving onto the next round...")
-    if userguess2 !=song:
-        print("Incorrect! Game Over!")
-          
-
-
-elif userguess1 == song:
     print("   ")
-    print("Correct! Moving onto the next round...")
+
+    songguess = random.choice(songs)
+
+    artist = songguess.split("-")
+    song = songguess.split("-")
+    print(artist[1])
+
+    songfirstchar = song[0]
+
+    print(songfirstchar[0])
+
+    # picks a random song and displays the first letter
+
+    print("   ")
+
+    userguess1 = input("What do you think the song is? : ")
+
+    if userguess1 != songfirstchar:
+        print("Incorrect! You have one more chance!")
+        print("  ")
+        userguess2 = input("What do you think the song is? : ")
+        print("  ")
+        if userguess2 == songfirstchar:
+         print("Correct! Moving onto the next round...")
+         if userguess2 !=songfirstchar:
+            print("Incorrect! Game Over!")
+            
+        
+            
+
+
+
+
+    elif userguess1 == songfirstchar:
+        print("   ")
+        print("Correct! Moving onto the next round...")
+
+        
+if userguess2 !=songfirstchar:
+    print("Incorrect! Game Over!")
